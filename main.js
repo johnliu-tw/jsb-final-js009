e7eb62eb155e0ed3a12b017e5028374b38de4f09
John Liu <john831118@gmail.com>
2017-04-08 15:28:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 //設定畫布環境
 var bgImg = document.createElement("img");
+var enemyImg = document.createElement("img");
 bgImg.src="images/map.png";
+enemyImg.src="images/jason.gif"
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
+//設定敵人
+var enemy ={
+   x:96,
+   y:480-32
+};
+
 function draw(){
    ctx.drawImage(bgImg,0,0);
+   ctx.drawImage(enemyImg,enemy.x,enemy.y)
 }
 
-setTimeout(draw,1000);
+
+
+setInterval(draw,16);
