bb1b544f0790fdb233712002bb43f364d90385e2
John Liu <john831118@gmail.com>
2017-05-13 15:44:41 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var bgImg = document.createElement("img");
 var enemyImg = document.createElement("img");
 var towerbtnImg= document.createElement("img")
 var towerImg= document.createElement("img") 
+var crosshairImage = document.createElement("img") 
 
+crosshairImage = "images/crosshair.png"
 bgImg.src="images/map.png";
 enemyImg.src="images/jason.gif"
 towerbtnImg.src="images/tower-btn.png"
@@ -14,6 +16,9 @@ var ctx = canvas.getContext("2d");
 var isBuild = false;
 var FPS = 60;
 var clock =0
+var HP = 100;
+ctx.fillStyle="white"
+ctx.font = "24px Arial"
 //設定敵人
 
 function Enemy(){
@@ -22,8 +27,16 @@ function Enemy(){
    this.speedX = 0;
    this.speedY = -64;
    this.pathDes = 0;
+   
+   this.hp = 10;
+   
    this.move = function(){
       if(isCollided(enemyPath[this.pathDes].x,enemyPath[this.pathDes].y,this.x,this.y,64/FPS,64/FPS)){
+         
+         if(this.pathDes===enemyPath.length-1){
+            this.hp = 0;
+            HP = HP - 10;
+         }
          
          this.x = enemyPath[this.pathDes].x;
          this.y = enemyPath[this.pathDes].y;      
@@ -72,7 +85,22 @@ var cursor = {
    y:0
 }
 
-var tower = {}
+var tower = {
+   range:96,
+   aimingEnemyId:null,
+   searchEnemy: function(){
+      for(var i=0;i<enemies.length;i++){
+         var distance =  Math.sqrt(
+             Math.pow(this.x - enemies[i].x,2) + Math.pow(this.y - enemies[i].y,2)
+         )
+         if(distance < this.range){
+            this.aimingEnemyId = i;
+            return;
+         }
+      }
+      this.aimingEnemyId = null;      
+   }
+}
 
 $("#game-canvas").on("mousemove",function(event){
       cursor.x = event.offsetX
@@ -100,14 +128,28 @@ function draw(){
       enemies.push(newEnemy);
    }
    for(var i = 0; i<enemies.length;i++){
+      
+      if(enemies[i].hp<1){
+         enemies.splice(i,1);
+      }
+      else{
       enemies[i].move();
       ctx.drawImage(enemyImg,enemies[i].x,enemies[i].y)
+      }
    }
    ctx.drawImage(towerbtnImg,560,432,48,48)
    if(isBuild){
       ctx.drawImage(towerImg,cursor.x,cursor.y)
    }
    ctx.drawImage(towerImg,tower.x,tower.y)
+   
+   tower._____();
+   if(tower._____!=null){
+      var id = tower.______;
+      ctx.drawImage(crosshairImage, enemies[__].x,enemies[__].y)
+   }
+   
+   ctx.fillText("HP: "+HP,20,20)
    
    clock = clock + 1;
    
