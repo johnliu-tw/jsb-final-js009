e506320d6ffe7e9c083a82e287a64c96e0951e11
John Liu <john831118@gmail.com>
2017-06-17 15:19:21 +0800

    Update main.js

--- main.js
+++ main.js
@@ -141,6 +141,7 @@ $("#game-canvas").on("click",function(event){
          money = money - 20  
          var newTower = new Tower(cursor.x-cursor.x%32 , cursor.y-cursor.y%32);
          towers.push(newTower);
+         isBuild = false;
         }
         else{
           isBuild = false;
